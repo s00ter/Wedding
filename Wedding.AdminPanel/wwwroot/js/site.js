5034d005161f70f28b42db25b2173500df6524b0
Vadzim.Vayadzilau <vadim@vadim.vadim>
2023-02-26 00:22:26 +0300

    [Admin] added rombus

--- Wedding.AdminPanel/wwwroot/js/site.js
+++ Wedding.AdminPanel/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const showRombus = () => {
+    console.log($('.preloader-wrapper'))
+    $('.preloader-wrapper').fadeOut();
+    $('.preloader-wrapper').removeAttr('hidden')
+}
+
+const hideRombus = () => {
+    $('.preloader-wrapper').fadeIn();
+    $('.preloader-wrapper').attr('hidden', 'hidden')
+}
